382452272331c5cf349565efe5eccb629ca9f9e1
Sameer Saifi <Sameer.edu19@gmail.com>
2025-11-27 14:50:35 +0530

    working fine

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -7,37 +7,37 @@ import { ConvexHttpClient } from "convex/browser";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-    secret: process.env.NEXT_PUBLIC_LIVEBLOCKS_SECRET_KEY!,
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
-    const authorization = await auth();
-    const user = await currentUser();
+  const authorization = await auth();
+  const user = await currentUser();
 
-    if (!authorization || !user) {
-        return new Response("Unauthorized", { status: 401 });
-    }
+  if (!authorization || !user) {
+    return new Response("Unauthorized", { status: 401 });
+  }
 
-    const { room } = await request.json();
-    const board = await convex.query(api.board.get, { id: room });
+  const { room } = await request.json();
+  const board = await convex.query(api.board.get, { id: room });
 
-    if (board?.orgId !== authorization.orgId) {
-        return new Response("Unauthorized", { status: 401 });
-    }
+  if (!board) {
+    return new Response("Board not found", { status: 404 });
+}
 
-    const userInfo = {
-        name: user.firstName || "Anonymous",
-        picture: user.imageUrl!,
-    };
+  const userInfo = {
+    name: user.firstName || "Anonymous",
+    picture: user.imageUrl!,
+  };
 
-    const session = liveblocks.prepareSession(user.id, {
-        userInfo,
-    });
+  const session = liveblocks.prepareSession(user.id, {
+    userInfo,
+  });
 
-    if (room) {
-        session.allow(room, session.FULL_ACCESS);
-    }
+  if (room) {
+    session.allow(room, session.FULL_ACCESS);
+  }
 
-    const { status, body } = await session.authorize();
-    return new Response(body, { status });
+  const { status, body } = await session.authorize();
+  return new Response(body, { status });
 }
--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://regular-humpback-73.clerk.accounts.dev",
+            domain: "https://verified-octopus-71.clerk.accounts.dev",
             applicationID: "convex",
         },
     ],
